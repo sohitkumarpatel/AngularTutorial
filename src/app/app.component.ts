d35dbb898e1b446567a28b4ab488d4677e73cbb7
sohitkumarpatel <sohit.patel9889@gmail.com>
2021-10-16 23:20:07 +0530

    4 files modified, 6 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,46 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular';
+export class AppComponent {
+  name = 'You are the best singer';
+  // show=false;
+  show="green";
+  color="pink";
+  currentVal = '';
+  disabledBox = true;
+  getVal(val) {
+    console.warn(val);
+    this.currentVal = val;
+  }
+  enableBox() {
+  this.disabledBox=false;
+  }
+// data=['sohit','Mansi','Rahul','Yogesh','Basanta','Jhabara'];
+data=[
+  {
+    name:'sohit',
+    age:23,
+    address:'Varanasi',
+    email:'sohit@gmail.com'
+  },
+  {
+    name:'Yogesh',
+    age:23,
+    address:'Mirzapur',
+    email:'yogesh@gmail.com'
+
+  },
+  {
+    name:'Garvit',
+    age:22,
+    address:'Jaunpur',
+    email:'garvit@gmail.com'
+
+  }
+]
+getUserValue(value){
+  console.warn(value);
+}
 }
